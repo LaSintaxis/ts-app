9dba0a4b929e07637791a584f092ced7a112a30c
LaSintaxis <nataliamariacardenasgarcia@gmail.com>
2025-09-08 17:15:44 -0500

    trabajando en que se vea el frontend en android studio

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,24 +1,24 @@
-module.exports = function(api) {
-    api.cache(true);
-    return {
-        presets: ['babel-preset-expo'],
-        plugins: [
-            'react-native-reanimed/plugin',
-            [
-                'module-resolve',
-                {
-                    root: ['./'],
-                    alias: {
-                        '@': './src',
-                        '@/components': './src/components',
-                        '@/screens': './src/screens',
-                        '@/services': './src/services',
-                        '@/types': './src/types',
-                        '@/utils': './src/utils',
-                        '@/contexts': './src/contexts'
-                    },
-                },
-            ],
-        ],
-    };
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-worklets/plugin', // 👈 ahora sí existe
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './src',
+            '@/components': './src/components',
+            '@/screens': './src/screens',
+            '@/services': './src/services',
+            '@/types': './src/types',
+            '@/utils': './src/utils',
+            '@/contexts': './src/contexts'
+          },
+        },
+      ],
+    ],
+  };
 };
